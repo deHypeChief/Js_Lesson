a9810c6ab4646721849081110260e2a1eec33c34
deHypeChief <dev.hype7@gmail.com>
2023-08-12 23:07:59 +0000

    getting started

---
+++ lessons/working-with-variables.js
@@ -0,0 +1,30 @@
+// << Working with variables >>
+// let myVariableName = <myData>
+
+// variables and their data types
+
+let myName = "Tomi😊"    //type: string
+let myPin = 1237       //type: number
+let isActive = true      //type: boolen
+let myBanks = ['GTB', 'KUDA', 'ZENITH', 'PAYWISE'] //type: array
+let myData = {
+    name: 'Tomi😊',
+    pin: 1237
+} //type: object
+
+
+// Homework 1
+// Create your own variables and their data type in the 
+// assigment folder. The data should be relative to bank data
+
+
+
+// << changing variables >>
+let myNewName = "David👌"
+
+// Note : to show your data on the terminal use "console.log()"
+
+
+console.log(myName); // output: Tomi😊
+
+myName = m
